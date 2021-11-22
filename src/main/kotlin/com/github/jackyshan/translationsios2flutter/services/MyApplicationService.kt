98fce9008d36b624eaa8267fd8ef79e5bababf48
GitHub Action <action@github.com>
2021-11-22 07:26:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jackyshan/translationsios2flutter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jackyshan.translationsios2flutter.services
+
+import com.github.jackyshan.translationsios2flutter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jackyshan/translationsios2flutter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jackyshan/translationsios2flutter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jackyshan.translationsios2flutter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jackyshan.translationsios2flutter.MyBundle
 
 class MyProjectService(project: Project) {
 
